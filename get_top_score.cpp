885a4e3c1f6eb22d9af960d646cf27e4dba81e8a
alexw11111 <westby.alex@yahoo.com>
2016-11-02 14:33:55 -0500

    Add files via upload

---
+++ get_top_score.cpp
@@ -0,0 +1,13 @@
+#include "std_lib_facilities_4.h"
+
+void put_score(ofstream ofs, ifstream ifs,score)
+{
+vector<score> high_score;
+for (int i=0; i<=4; ++i)
+{
+	ifs>>high_score[i].score>>high_score[i].initials
+}
+
+
+
+}
