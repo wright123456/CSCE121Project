9a8d887a6b23ddc5a4c45f6c76a47414a0d4c1eb
alexw11111 <westby.alex@yahoo.com>
2016-12-03 15:44:03 -0600

    Add files via upload

--- get_top_score.cpp
+++ get_top_score.cpp
@@ -4,16 +4,36 @@
 #include "GUI.h"
 #include "Graph.h"
 #include "Point.h"
+#include "splash_screen.h"
 
 
+float user_score;
+string user_initials = "hello";
+int scoreboard_number = 6;
+int get_scoreboard_number()
+{
+	return scoreboard_number;
+}
 
-bool final_window=true;
+void set_user_score(float score_a)
+{
+	user_score = score_a;
+}
+
+bool final_window = true;
+void final_window_truth(bool truth_update)
+{
+	final_window = truth_update;
+}
 
 
 score::score(string initials, float score_num)
 	:initials(initials), score_num(score_num) {}
 score::score()
-	:initials("N/A"), score_num(0) {}
+{
+	initials = "N/A";
+	score_num = 0;
+}
 
 score::~score() {}
 
@@ -31,10 +51,7 @@ void update_high_score(vector<score>& scoreboard, score& current)
 		}
 	}
 }
-
-
-
-vector<score> update_scoreboard(int scoreboard_num, score current, bool update)
+vector<score> scoreboard_input(int scoreboard_num)
 {
 	score temp{ "AJW", 15000 };
 	float single_score;
@@ -43,24 +60,42 @@ vector<score> update_scoreboard(int scoreboard_num, score current, bool update)
 	vector<string> initials_temp;
 	vector<score> high_score;
 	vector<string> scoreboard_names = { "scoreboard_3.txt", "scoreboard_4.txt", "scoreboard_5.txt",
-		"scoreboard_5.txt","scoreboard_6.txt","scoreboard_7.txt" }; // Scoreboards for different difficulties
-	ifstream ifstr{ scoreboard_names[scoreboard_num - 3] };
+		"scoreboard_5.txt","scoreboard_6.txt","scoreboard_7.txt" };
+	ifstream ifstr{ scoreboard_names[scoreboard_num - 2] };
+	if (ifstr.fail())
+		throw bad_scoreboard_num{};
 	for (unsigned int i = 0; i <= 4; ++i) // Reading top five scores to a vector
 	{
 		ifstr >> single_initials >> single_score;
+		if (single_initials == "")
+			single_initials = "N/A";
 		initials_temp.push_back(single_initials);
 		score_temp.push_back(single_score);
 		temp = score{ initials_temp[i],score_temp[i] };
 		high_score.push_back(temp);
 	}
 	ifstr.close();
-	ofstream ofstr{ scoreboard_names[scoreboard_num - 3] };
+	return high_score;
+}
+
+vector<score> scoreboard_output(score current, int scoreboard_num, bool update, vector<score> high_score)
+{
+	vector<string> scoreboard_names = { "scoreboard_3.txt", "scoreboard_4.txt", "scoreboard_5.txt",
+	"scoreboard_5.txt","scoreboard_6.txt","scoreboard_7.txt" };
+	ofstream ofstr{ scoreboard_names[scoreboard_num - 2] };
 	if (update == true)
 		update_high_score(high_score, current); // If it is a high score, it is placed in the vector
 	for (unsigned int i = 0; i <= high_score.size() - 1; ++i) // Writing to the save file
 	{
 		ofstr << high_score[i].get_initials() << "      " << high_score[i].get_score_num() << std::endl;
 	}
+	return high_score;
+}
+
+vector<score> update_scoreboard(int scoreboard_num, score current, bool update)
+{
+	vector<score> high_score = scoreboard_input(scoreboard_num);
+	high_score = scoreboard_output(current, scoreboard_num, update, high_score);
 	return high_score;
 }
 
@@ -73,11 +108,16 @@ Score_Window::Score_Window(Point xy, int w, int h, const string& title)
 		[](Address, Address pw) {reference_to<Score_Window>(pw).next(); } },
 	quit_button{ Point{0,0 }, 120, 50, "Quit",
 		[](Address, Address pw) {reference_to<Score_Window>(pw).quit(); } },
-	scoreboard_box{ Point{500,350  }, 120, 50, "Scoreboard Number(3-7)" },
+	again_button{ Point{880,0 }, 120, 50, "Play Again",
+		[](Address, Address pw) {reference_to<Score_Window>(pw).again(); } },
+	scoreboard_next_button{ Point{0,0}, 120, 50, "Next",
+		[](Address, Address pw) {reference_to<Score_Window>(pw).scoreboard_next(); } },
+	scoreboard_box{ Point{500,350  }, 120, 50, " Difficulty(3-7)" },
 	initials_box{ Point{ 500,500 }, 120, 50, "Initials" },
 	score_text{ Point{600,50},"Score" },
-	initials_text{ Point{400,50},"Initials" }
-	
+	initials_text{ Point{400,50},"Initials" },
+	initials_text_current{ Point{ 400,400 },user_initials },
+	score_text_current{ Point{ 600,400 },to_string(user_score) }
 
 
 
@@ -94,37 +134,66 @@ Score_Window::Score_Window(Point xy, int w, int h, const string& title)
 	}
 }
 
-
+void Score_Window::attach_scores()
+{
+	score John = { user_initials,user_score };
+	Vector<Text*> scores_text_vec;
+	Vector<Text*> initials_text_vec;
+	Vector<score> scoreboard = update_scoreboard(scoreboard_number, John, false);
+	for (unsigned int i = 0; i <= 4; ++i)
+	{
+		scores_text_vec.push_back(new Text{ Point{ 600,50u * i + 100u },to_string(scoreboard[i].get_score_num()) });
+		initials_text_vec.push_back(new Text{ Point{400,50u * i + 100u},scoreboard[i].get_initials() });
+		attach(*(scores_text_vec[scores_text_vec.size() - 1]));
+		attach(*(initials_text_vec[initials_text_vec.size() - 1]));
+	}
+	redraw();
+}
 
 void Score_Window::next()
 {
 	if (final_window == false)
 	{
-		int scoreboard_number = scoreboard_box.get_int();
-		string initials_get{ initials_box.get_string() };
-		attach(*(new Text{ Point{400,400},initials_get }));
+		scoreboard_number = scoreboard_box.get_int();
+		user_initials = initials_box.get_string();
+		cout << initials_a;
+		attach(*(new Text{ Point{400,400},user_initials }));
 		detach(initials_box);
 		detach(scoreboard_box);
 		detach(next_button);
+		attach(scoreboard_next_button);
 	}
-	attach(quit_button);
+	else
+	{
+		attach(again_button);
+		attach(quit_button);
+		attach(initials_text_current);
+		attach(score_text_current);
+	}
 	attach(score_text);
 	attach(initials_text);
-	score John = { "ast",55 };
-	Vector<Text*> scores_text;
-	Vector<Text*> initials_text;
-	Vector<score> scoreboard = update_scoreboard(3, John, false);
-	for (unsigned int i = 0; i <= 4; ++i)
-	{
-		scores_text.push_back(new Text{ Point{600,50 * i + 100},to_string(scoreboard[i].get_score_num()) });
-		initials_text.push_back(new Text{ Point{400,50 * i + 100},scoreboard[i].get_initials() });
-		attach(*(scores_text[scores_text.size() - 1]));
-		attach(*(initials_text[initials_text.size() - 1]));
-	}
-	redraw();
+	attach_scores();
 }
 void Score_Window::quit()
 {
+	score John = { user_initials,user_score };
+	Vector<score> scoreboard2 = update_scoreboard(scoreboard_number, John, true);
 	hide();
+
 }
 
+int Score_Window::again()
+{
+	score John = { user_initials,user_score };
+	Vector<score> scoreboard2 = update_scoreboard(scoreboard_number, John, true);
+	hide();
+	//Insert for window to start again
+	Splash_window begin{Point{100,100},1000,700,"" };
+	return gui_main();
+}
+
+void Score_Window::scoreboard_next()
+{
+	hide();
+	//Insert code for next window/to start game
+}
--- get_top_score.h
+++ get_top_score.h
@@ -5,7 +5,13 @@
 #include "GUI.h"
 #include "Graph.h"
 #include "Point.h"
-extern bool final_window;
+
+
+int get_scoreboard_number();
+void set_user_score(float);
+void final_window_truth(bool truth_update);
+
+class bad_scoreboard_num{};
 
 class score
 {
@@ -33,24 +39,29 @@ struct Score_Window :Graph_lib::Window
 {
 	Score_Window(Point xy, int w, int h, const string& title);
 	float get_user_score(float score);
+	string initials_a;
 	
 private:
 	Vector<score> scoreboard;
 	Graph_lib::Button next_button;
 	Graph_lib::Button quit_button;
+	Graph_lib::Button again_button;
+	Graph_lib::Button scoreboard_next_button;
 	In_box scoreboard_box;
 	In_box initials_box;
 	void next();
 	void quit();
-	Text  initials_text, score_text;
-	string initials_get;
-
-	score user;
+	int again();
+	void scoreboard_next();
+	void attach_scores();
+	Text  initials_text, score_text, score_text_current, initials_text_current;
 };
 
 
 
 void update_high_score(vector<score>& scoreboard, score& current);
+vector<score> scoreboard_output(score current, int scoreboard_num, bool update, vector<score> high_score);
+vector<score> scoreboard_input(int scoreboard_num);
 
 Vector<score> update_scoreboard(int scoreboard_num, score current);
 
